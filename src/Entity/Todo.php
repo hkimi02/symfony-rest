ed5adb8a8863030f72a0bcfa297559c5d53fdf5d
hkimi amin <hkimiamin02@gmail.com>
2024-01-15 18:03:23 +0100

    add migration

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -13,8 +13,38 @@ class Todo
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
